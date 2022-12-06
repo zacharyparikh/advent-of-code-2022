46e401c27750262a04f63a1672cac44d2f962cfc
Zachary Parikh <parikhzachary@gmail.com>
2022-12-06 12:07:59 -0500

    Day 6 Iterator instead of recursion

--- src/day6.rs
+++ src/day6.rs
@@ -4,33 +4,34 @@ use std::{
     str::Chars,
 };
 
-fn find_aux(
-    mut stream: Chars,
-    mut last_chars: VecDeque<char>,
-    current_index: usize,
-    num_chars: usize,
-) -> Option<usize> {
-    let current = stream.next()?;
-    last_chars.push_back(current);
+fn find_aux(mut stream: Chars, num_chars: usize) -> usize {
+    let mut last_chars = VecDeque::new();
+    let index = stream
+        .position(|current| {
+            last_chars.push_back(current);
 
-    if last_chars.len() == num_chars {
-        let char_set: HashSet<&char> = HashSet::from_iter(last_chars.iter());
-        if char_set.len() == num_chars {
-            return Some(current_index);
-        } else {
-            last_chars.pop_front();
-        }
-    }
+            if last_chars.len() == num_chars {
+                let char_set: HashSet<&char> = HashSet::from_iter(last_chars.iter());
+                if char_set.len() == num_chars {
+                    return true;
+                } else {
+                    last_chars.pop_front();
+                }
+            }
 
-    find_aux(stream, last_chars, current_index + 1, num_chars)
+            false
+        })
+        .unwrap();
+
+    index + 1
 }
 
 fn find_start_of_packet(stream: Chars) -> usize {
-    find_aux(stream, VecDeque::new(), 1, 4).unwrap()
+    find_aux(stream, 4)
 }
 
 fn find_start_of_message(stream: Chars) -> usize {
-    find_aux(stream, VecDeque::new(), 1, 14).unwrap()
+    find_aux(stream, 14)
 }
 
 pub fn part1() -> usize {
